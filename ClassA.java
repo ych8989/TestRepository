fe51cab28031712c0ca53201f7bba60cc9d0074f
ych8989 <ych8989@naver.com>
2017-04-24 14:07:58 +0900

    연습 파일 삭제

--- ClassA.java
+++
@@ -1,8 +0,0 @@
-public class ClassA{
-
-  public static void main(String[] args) {
- System.out..println("user1 작성");
-  System.out.println("아무거나");
-
-  }
-}
--- ClassB.java
+++
@@ -1,6 +0,0 @@
-public class ClassA{
-  public static void public static void main(String[] args) {
-    System.out.println("user2 작성");
-
-  }
-}
