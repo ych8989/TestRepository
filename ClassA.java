c0842d549b174d155334a3d3079f4828b96713e1
ych8989 <ych8989@naver.com>
2017-04-24 12:43:16 +0900

    수정함

--- ClassA.java
+++ ClassA.java
@@ -1,5 +1,6 @@
 public class ClassA{
-  public static void public static void main(String[] args) {
-    
+  public static void main(String[] args) {
+ System.out.System.out.println("user1 작성");
+   
   }
 }
